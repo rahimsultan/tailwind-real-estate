e5d9ff1fe4aab3bb7042cd7792dc5ade2b2aaaee
rahimsultan <sulrahim3@gmail.com>
2023-07-15 15:38:41 +0600

    service

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        black:{
+          900: '#000000',
+          dark: '#242527',
+          light: '#2B2B2B'
+        },
+        gray:{
+          dark: '#C4C4C4',
+          light: '#E5E5E5'
+        },
+        blue:{
+          light: '#2387C0',
+          dark: '#0A72AD',
+        }
+      }
+    },
   },
   plugins: [],
 }
